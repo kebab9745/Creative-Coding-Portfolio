93c01bac170877a480545e96a0428df351f473df
Omar <omar.abdl.hnn@gmail.com>
2025-05-28 22:37:15 +0400

    Add files via upload

---
+++ Pointillism Effect/sketch.js
@@ -0,0 +1,20 @@
+var img, x, y;
+
+function preload() {
+  img = loadImage("tree.jpg");
+}
+
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+  noStroke();
+  img.resize(width, height);
+}
+
+function draw() {
+  x = random(width);
+  y = random(height);
+  var c = img.get(x, y); 
+  fill(c[0], c[1], c[2], 50); 
+  ellipse(x, y, 30, 30);
+}
